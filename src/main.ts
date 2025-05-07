d5513da1eeaf373da62c8a4a2a3e7b6336dc4c67
rti <it.development.r@gmail.com>
2025-05-07 15:37:01 +0400

    финал 1

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+	const configService = new ConfigService();
+	const app = await NestFactory.create(AppModule);
+	app.setGlobalPrefix('api/v1');
+	app.enableCors({
+		origin: configService.get('ALLOW_HOST'),
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		credentials: true,
+	});
+	await app.listen(3000);
+}
+bootstrap();
