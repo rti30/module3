bfda946f8ddca6eefb41765121ee6a2017a8b806
Siporoz <solovev.nikita@mail.ru>
2025-05-19 15:34:04 +0500

    Remove all files

--- src/score/dto/update-score.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateScoreDto } from './create-score.dto';
-import { IsString } from 'class-validator';
-
-export class UpdateScoreDto extends PartialType(CreateScoreDto) {
-	@IsString()
-	startTime: Date;
-}
